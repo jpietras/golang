a9ce7111464bc7fdb7686c1014c8c9e5e3eb17b2
Joseph C. Pietras <jpietras@oreillyauto.com>
2018-10-26 15:03:54 -0500

    Creating a space for go code.

---
+++ firstAttempts/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+       "fmt"
+       "github.com/jpietras/string"
+       )
+
+func main() {
+	fmt.Println("hello, world")
+	fmt.Println(string.Reverse("hello, world"))
+}
